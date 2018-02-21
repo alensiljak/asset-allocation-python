0c25797faf32465a3935ded3e430250359ae813d
Alen Siljak <alen.siljak@gmx.com>
2018-02-21 16:49:02 +0100

    aligning the column in the text output to fit a spreadsheet, vertical separator

--- asset_allocation/formatters.py
+++ asset_allocation/formatters.py
@@ -8,7 +8,7 @@ class AsciiFormatter:
     """ Formats the model for the console output """
     def __init__(self):
         self.columns = [ 
-            { "name": "Asset Class", "width": 20 }, 
+            { "name": "Asset Class", "width": 25 }, 
             { "name": "allocation", "width": 5 },
             { "name": "value", "width": 10 }
         ]
@@ -20,11 +20,12 @@ class AsciiFormatter:
 
         # Header
         output = f"Asset Allocation model, total: {model.currency} {model.total_amount:,.2f}\n"
-        # Columns
+        # Column Headers
         for column in self.columns:
             width = column["width"]
             output += f"{column['name']:^{width}}"
         output += "\n"
+        output = f"---------------------------------------------------------------\n"
 
         # Asset classes
         for ac in model.classes:
@@ -47,19 +48,22 @@ class AsciiFormatter:
     def __get_ac_row(self, ac: AssetClass):
         """ Formats one Asset Class record """
         output = ""
+
+        # Name
+        name_col = ac.name
         # Indent according to depth.
         for _ in range(0, ac.depth):
-            output = f"    {output}"
+            name_col = f"    {name_col}"
         
         width = self.columns[0]["width"]
-        output += f"{ac.name:<{width}}: "
+        output += f"{name_col:<{width}}: "
         
         allocation = f"{ac.allocation:.2f}"
         output += f"{allocation:>5}"
 
         # value
         value = f"{ac.curr_value:,.0f}"
-        output += f"{value:>10}"
+        output += f"{value:>9}"
 
         # https://en.wikipedia.org/wiki/ANSI_escape_code
         # CSI="\x1B["
@@ -71,18 +75,21 @@ class AsciiFormatter:
     def __get_stock_row(self, stock: Stock, depth: int) -> str:
         """ formats stock row """
         output = ""
-        for _ in range(0, depth):
-            output += "    "
-        width = self.columns[0]["width"]
-        output += f"{stock.symbol:<{width}}: "
 
-        #stock.
+        # Symbol
+        name_col = stock.symbol
+        for _ in range(0, depth):
+            name_col = f"    {name_col}"
+        width = self.columns[0]["width"]
+        output += f"{name_col:<{width}}: "
+
+        # Current allocation
         allocation = f"{stock.curr_alloc:.2f}"
         output += f"{allocation:>5}"
 
         # Value in base currency
         value = f"{stock.value_in_base_currency:,.0f}"
-        output += f"{value:>8}"
+        output += f"{value:>9}"
 
         # Value in security's currency.
         value = f"({stock.value:,.0f}"
