5e7fa43e3935aabf3e2774e1b42afbf30d472815
Alen Siljak <alen.siljak@gmx.com>
2018-03-03 19:56:52 +0100

    including data files in the package

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.2.0',  # Required
+    version='0.2.1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -175,4 +175,8 @@ setup(
             'aa=asset_allocation.cli:cli',
         ],
     },
+
+    # Include non-code files, listed in MANIFEST.in.
+    # http://python-packaging.readthedocs.io/en/latest/non-code-files.html
+    include_package_data=True
 )
